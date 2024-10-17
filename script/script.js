b4dfce770f20243b2d4c9c17800dfc1a5a44a643
Daniel Andre <p.danielAndre12@gmail.com>
2024-10-17 22:13:22 +0800

    Add files via upload
    try

---
+++ script/script.js
@@ -0,0 +1,7 @@
+document.getElementById('login-btn').addEventListener('click', function() {
+  window.location.href = '/login'; // Replace with your login page URL
+});
+
+document.getElementById('get-started-btn').addEventListener('click', function() {
+  window.location.href = '/get-started'; // Replace with your sign-up page URL
+});
