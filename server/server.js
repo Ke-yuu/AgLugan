89a8d17f0bcbc9a79a053f4677b6c5c3f3343cd7
Josh Rabino <rabino.josh@gmail.com>
2024-12-11 02:02:51 +0800

    fixed user_type registration

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://192.168.0.119:3000'],
+    origin: ['http://localhost:3000', 'http://192.168.1.19:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
